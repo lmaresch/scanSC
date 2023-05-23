d31ef0701e312ecfcc889ca38c5ddc6b0cc5b1cc
Libor Maresch <libor.maresch@sap.com>
2023-05-23 16:56:23 +0200

    Try to retrieve larger cover

--- scanSC.py
+++ scanSC.py
@@ -12,8 +12,18 @@ import re
 wd = os.getcwd()
 baseUrl = 'https://soundcloud.com'
 searchUrl = "{0}/search?q=".format(baseUrl)
+overrideCover = False
+print(sys.argv)
 if __name__ == '__main__' and len(sys.argv) > 1:
-    wd = sys.argv[1]
+    for arg in sys.argv:
+        if arg.startswith('-'):
+            if arg == '-O':
+                overrideCover = True
+        elif arg == sys.argv[0]:
+            continue
+        else:
+            wd = arg
+            break
 print("Working in directory {0}".format(wd))
 files = [f for f in glob.glob(os.path.join(wd, '**', '*.mp3'), recursive=True)]
 notWorking = []
@@ -31,7 +41,7 @@ for f in files:
         continue
     print("Artist: {0} - song: {1}".format(mp3.tag.artist, mp3.tag.title))
     print("Cover: {0}".format(len(mp3.tag.images)))
-    if len(mp3.tag.images) > 0:
+    if not overrideCover and len(mp3.tag.images) > 0:
         print('Cover already exists, proceeding to next file')
         continue
     httpSearch = urllib.parse.quote("{0} {1}".format(mp3.tag.artist, re.sub(" [\[\(].*[\]\)]", "", mp3.tag.title)))
@@ -52,7 +62,7 @@ for f in files:
             link = xmldict['li']['h2']['a']['@href']
             song = xmldict['li']['h2']['a']['#text']
             entries.append(dict(url = link, song = song))
-            print("{0}: {1}".format(i, song))
+            print("{0}: {1} -- {2}".format(i, song, link))
             i += 1
     selectedEntry = -1
     if len(entries) < 1:
@@ -84,7 +94,12 @@ for f in files:
         continue
     cover = None
     jsonString = None
+    bigPicUrl = ''
+    smallPicUrl = ''
     for line in response.data.decode('utf-8').split('\n'):
+        if '<img src="' in line:
+            bigPicUrl = re.sub('".*', '', line.strip().replace('<img src="', ''))
+            print('Big picture: {0}'.format(bigPicUrl))
         if '<script>window.__sc_hydration = ' in line:
             jsonString = line.replace('<script>window.__sc_hydration = ', '').replace(';</script>', '')
     jsonObj = json.loads(jsonString)
@@ -93,14 +108,8 @@ for f in files:
             data = entry['data']
             for tagName in data.keys():
                 if tagName == 'artwork_url' and data[tagName] is not None:
-                    print(data[tagName])
-                    mimeType = 'jpeg'
-                    if '.png' in data[tagName]:
-                        mimeType = 'png'
-                    http = urllib3.PoolManager()
-                    r = http.request(method = 'GET', url = data[tagName])
-                    mp3.tag.images.set(3, r.data, 'image/{0}'.format(mimeType))
-                    mp3.tag.save()
+                    smallPicUrl = data[tagName]
+                    print('Small picture: {0}'.format(smallPicUrl))
                 elif tagName == 'created_at':
                     print(data[tagName])
                     mp3.tag.release_date = data[tagName][0:4]
@@ -111,3 +120,18 @@ for f in files:
                     mp3.tag.save()
                 elif tagName == 'label_name':
                     print(data[tagName])
+    picUrl = ''
+    if len(bigPicUrl) > 0:
+        picUrl = bigPicUrl
+        print('Using big picture')
+    elif len(smallPicUrl) > 0:
+        picUrl = smallPicUrl
+        print('Using small picture')
+    if len(picUrl) > 0:
+        mimeType = 'jpeg'
+        if '.png' in picUrl:
+            mimeType = 'png'
+        http = urllib3.PoolManager()
+        r = http.request(method = 'GET', url = picUrl)
+        mp3.tag.images.set(3, r.data, 'image/{0}'.format(mimeType))
+        mp3.tag.save()
